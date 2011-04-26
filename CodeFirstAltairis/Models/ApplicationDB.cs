cd83cade4f98cbeba898d586a1eb64d73ba4db23
James Culbertson <info@turnkey-commerce.com>
2011-04-25 20:46:27 -0500

    Add automatic initialization of users and their roles.

--- CodeFirstAltairis/Models/ApplicationDB.cs
+++ CodeFirstAltairis/Models/ApplicationDB.cs
@@ -4,6 +4,8 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using CodeFirstAltairis.Models;
+using System.Web.Security;
+using System.Data;
 
 namespace CodeFirstAltairis.Models
 {
@@ -24,6 +26,7 @@ namespace CodeFirstAltairis.Models
                 m.MapRightKey("RoleName");
             });
         }
+
     }
 
     // Change the base class as follows if you want to drop and create the database during development:
@@ -32,13 +35,33 @@ namespace CodeFirstAltairis.Models
     {
         protected override void Seed(ApplicationDB context) 
         {
+            // Create some roles.
             var roles = new List<Role>{
                 new Role{RoleName = "Administrator"},
                 new Role{RoleName = "User"},
                 new Role{RoleName = "PowerUser"}
             };
-
             roles.ForEach(r => context.Roles.Add(r));
+
+            // Create some users.
+            MembershipCreateStatus status = new MembershipCreateStatus();
+            Membership.CreateUser("admin", "password", "admin@user.com");
+            if (status == MembershipCreateStatus.Success) {
+                // Add the role.
+                User admin = context.Users.Find("admin");
+                Role adminRole = context.Roles.Find("Administrator");
+                admin.Roles = new List<Role>();
+                admin.Roles.Add(adminRole);
+            }
+            Membership.CreateUser("user", "password", "user@user.com");
+            if (status == MembershipCreateStatus.Success) {
+                // Add the role.
+                User user = context.Users.Find("user");
+                Role userRole = context.Roles.Find("User");
+                user.Roles = new List<Role>();
+                user.Roles.Add(userRole);
+            }
+            
         }
     }
 }
